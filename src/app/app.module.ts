1e40486fef8a9c4a240ead45387cf50e666b4e1b
rafikidota <lesmes240698@gmail.com>
2022-08-11 01:32:20 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from './auth/auth.module';
 import { PagesModule } from './pages/pages.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { NotPageFoundComponent } from './not-page-found/not-page-found.component';
@@ -17,8 +18,9 @@ import { NotPageFoundComponent } from './not-page-found/not-page-found.component
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AuthModule,
     PagesModule,
-    AuthModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
