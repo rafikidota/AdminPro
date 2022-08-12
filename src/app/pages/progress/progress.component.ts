12bd85f3c03ad4728fc70d988e281c7269af2d0c
rafikidota <lesmes240698@gmail.com>
2022-08-12 01:56:09 -0400

    template added to progress bar

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgressComponent implements OnInit {
 
+  progress1: number = 25;
+  progress2: number = 35;
+
+  getProgress1() {
+    return `${this.progress1}%`;
+  }
+  getProgress2() {
+    return `${this.progress2}%`;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  changeProgress(value: number) {
+    this.progress1 = value;
+  }
 }
