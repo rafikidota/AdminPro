01d7e7ea5357ae02364fa5d7e8a0773a861e2d4e
rafikidota <lesmes240698@gmail.com>
2022-08-16 01:00:30 -0400

    template added to doughnut

--- src/app/components/doughnut/doughnut.component.ts
+++ src/app/components/doughnut/doughnut.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ChartData, ChartType, ChartConfiguration, ChartEvent } from 'chart.js';
 
 @Component({
   selector: 'app-doughnut',
@@ -6,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoughnutComponent implements OnInit {
 
+  @Input() title: string = 'Untitled';
+  @Input() colors: string[] = ['red', 'blue', 'violet'];
+  @Input() labels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  @Input() data: number[] = [350, 450, 100];
+
+  // Doughnut
+  public doughnutChartLabels: string[] = this.labels;
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: this.data, backgroundColor: this.colors },
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+  };
+
+  // events
+  public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+    this.doughnutChartData.labels = this.labels;
+    this.doughnutChartData.datasets[0].backgroundColor = this.colors;
+    this.doughnutChartData.datasets[0].data = this.data;
   }
 
 }
