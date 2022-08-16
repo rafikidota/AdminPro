bd7f3a6b6240c0925be6ce86cb9226076a50cae4
rafikidota <lesmes240698@gmail.com>
2022-08-16 03:36:57 -0400

    template added to account-settings

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from '../../services/settings.service';
 
 @Component({
   selector: 'app-account-settings',
@@ -6,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private ss: SettingsService
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { 
+    this.ss.checkCurrentTheme();
   }
 
+  changeTheme(theme: string) {
+    this.ss.changeTheme(theme);
+  }
 }
--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -5,5 +5,30 @@ import { Injectable } from '@angular/core';
 })
 export class SettingsService {
 
-  constructor() { }
+  private linkTheme = document.querySelector('#theme');
+  constructor() {
+    const url = localStorage.getItem('theme') || './assets/css/colors/purple-dark.css';
+    this.linkTheme?.setAttribute('href', url);
+  }
+
+  changeTheme(theme: string) {
+    const url = `./assets/css/colors/${theme}.css`;
+    this.linkTheme?.setAttribute('href', url);
+    localStorage.setItem('theme', url);
+    this.checkCurrentTheme();
+  }
+
+  checkCurrentTheme() {
+    const links = document.querySelectorAll('.selector');
+    links.forEach(elem => {
+      elem.classList.remove('working');
+      const btnTheme = elem.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+      const currentTheme = this.linkTheme?.getAttribute('href');
+      if (btnThemeUrl === currentTheme) {
+        elem.classList.add('working');
+      }
+    });
+  }
+
 }
