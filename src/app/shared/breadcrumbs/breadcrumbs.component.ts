dda4c20d99afe7c381471ba2df2d1ff819f64305
rafikidota <lesmes240698@gmail.com>
2022-08-17 19:08:42 -0400

    route data added to breadcrumbs

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,14 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { filter, map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.component.html'
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public title: string = '';
+  public routeData!: Subscription;
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router
+  ) {
+    this.getRouteData();
   }
+
+  ngOnInit(): void { }
+
+  ngOnDestroy(): void {
+    this.routeData.unsubscribe();
+  }
+
+  getRouteData() {
+    this.routeData = this.router.events
+      .pipe(
+        filter((event: any) => event instanceof ActivationEnd),
+        filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+        map((event: ActivationEnd) => event.snapshot.data),
+      )
+      .subscribe(data => {
+        this.title = data['title'];
+        document.title = `AdminPro - ${this.title}`;
+      });
+  }
+
 
 }
