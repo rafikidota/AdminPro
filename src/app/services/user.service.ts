e44268d8ef8d542bcf6c18396b8d7f219b61fe35
rafikidota <lesmes240698@gmail.com>
2022-08-25 22:47:01 -0400

    Create user.service.ts

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { RegisterForm } from '../interfaces/register-form.interface';
+import { UserResponse } from '../interfaces/user-response.interface';
+import { User } from '../models/user.models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  base_url = environment.base_url;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  register(user:User) {
+    return this.http.post<UserResponse>(`${this.base_url}/users`,user);
+  }
+}
