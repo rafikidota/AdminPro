7589b99b72d16b52f04189452fe70ee9dc531ec4
rafikidota <lesmes240698@gmail.com>
2022-08-25 23:55:10 -0400

    sign in added

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import sweetalert from 'sweetalert2';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -15,17 +17,41 @@ export class LoginComponent implements OnInit {
     remember_me: [false, []],
   });
 
+  submitted = false;
+
   constructor(
     private fb: FormBuilder,
-    private router: Router
+    private router: Router,
+    private auth: AuthService
   ) { }
 
   ngOnInit(): void {
     document.title = `AdminPro - Login`;
   }
-  
-  login(){
-    this.router.navigateByUrl('/');
+
+  login() {
+    this.submitted = true;
+    if (this.hasErrors()) {
+      this.showSweetAlert();
+    } else {
+      const { email, password } = this.loginForm.value;
+      this.auth.login(email!, password!).subscribe({
+        next: (res) => {
+          if (res.ok === true) {
+            this.router.navigateByUrl('/');
+          }
+        },
+        error: (err) => {
+          sweetalert.fire('Error', err.error.msg, 'error');
+        }
+      });
+    }
+  }
+  hasErrors(){
+    return true;
+  }
+  showSweetAlert(){
+
   }
 
 }
