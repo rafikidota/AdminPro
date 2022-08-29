ccf7d5fb2ab3e7d0a4f26a8d2d9c2ae359c4dd19
rafikidota <lesmes240698@gmail.com>
2022-08-29 09:39:44 -0400

    reactives forms added

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user.models';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  public profileForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private us: UserService,
+    private as: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.profileForm = this.fb.group({
+      name: [this.as.user.name, [Validators.required]],
+      email: [this.as.user.email, [Validators.required, Validators.email]],
+      // password: ['', [Validators.required, Validators.minLength(6)]],
+      // confirm: ['', [Validators.required, Validators.minLength(6)]],
+      // terms: [false, [Validators.requiredTrue]],
+    });
+  }
+
+  save() {
+    const name = this.profileForm.get('name')?.value;
+    const email = this.profileForm.get('email')?.value;
+    const user = this.as.user;
+    user.name = name;
+    user.email = email;
+    this.us.update(user).subscribe( res =>{
+      this.as.user = res.user!
+    });
   }
 
 }
