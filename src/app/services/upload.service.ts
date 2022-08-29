4f0802e6aeffeb92036e8de3762960d3e7df6c54
rafikidota <lesmes240698@gmail.com>
2022-08-29 19:51:25 -0400

    updateImage( ... ) added

--- src/app/services/upload.service.ts
+++ src/app/services/upload.service.ts
@@ -1,9 +1,31 @@
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { UploadResponse } from '../interfaces/upload-response.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UploadService {
 
-  constructor() { }
+  base_url = environment.base_url;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  updateImage(
+    file: File,
+    collection: 'doctors' | 'hospitals' | 'users',
+    id: string
+  ) {
+    const url = `${this.base_url}/upload/${collection}/${id}`;
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('token', token || '');
+    const formData = new FormData();
+    formData.append('image', file, file.name);
+    const body = formData;
+    return this.http.put<UploadResponse>(url, body, { headers });
+  }
+  
 }
