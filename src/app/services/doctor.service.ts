8eb11b6f5369b6d656db29d42295276a9cc45772
rafikidota <lesmes240698@gmail.com>
2022-09-10 21:33:46 -0400

    Create doctor.service.ts

---
+++ src/app/services/doctor.service.ts
@@ -0,0 +1,57 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { DoctorResponse } from '../interfaces/doctor-response.interface';
+import { DoctorsResponse } from '../interfaces/get-doctors.interface';
+import { HospitalsResponse } from '../interfaces/get-hospitals.interface';
+import { HospitalResponse } from '../interfaces/hospital-response.interface';
+import { Doctor } from '../models/doctor.model';
+import { Hospital } from '../models/hospital.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DoctorService {
+
+  base_url = environment.base_url;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getDoctors(skip: number = 0, limit: number = 0) {
+    const url = `${this.base_url}/doctors?skip=${skip}&limit=${limit}`;
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('token', token || '');
+    return this.http.get<DoctorsResponse>(url, { headers })
+      .pipe(
+        map(res => {
+          const doctors = res.doctors?.map(doctor => new Doctor(doctor.name, doctor.hospital, doctor.id, doctor.user, doctor.img));
+          res.doctors = doctors;
+          return res;
+        })
+      );
+  }
+
+  create(doctor: { name: string, hospital: string }) {
+    const url = `${this.base_url}/doctors`;
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('token', token || '');
+    return this.http.post<DoctorResponse>(url, doctor, { headers });
+  }
+
+  update(doctor: Doctor) {
+    const url = `${this.base_url}/doctors`;
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('token', token || '');
+    return this.http.put<DoctorResponse>(url, doctor, { headers });
+  }
+
+  delete(doctor: Doctor) {
+    const url = `${this.base_url}/doctors/${doctor.id}`;
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders().set('token', token || '');
+    return this.http.delete<DoctorResponse>(url, { headers });
+  }
+}
